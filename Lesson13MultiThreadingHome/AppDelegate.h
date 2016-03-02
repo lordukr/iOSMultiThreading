a2540f789e86b6a29b2a3d8d54ed344ab556136b
Anatoli Zavialov <mymac@macbook-pro.local>
2016-03-03 00:01:21 +0200

    Initial Commit

---
+++ Lesson13MultiThreadingHome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lesson13MultiThreadingHome
+//
+//  Created by My mac on 03.03.16.
+//  Copyright © 2016 Anatolii Zavialov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lesson13MultiThreadingHome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lesson13MultiThreadingHome
+//
+//  Created by My mac on 03.03.16.
+//  Copyright © 2016 Anatolii Zavialov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
